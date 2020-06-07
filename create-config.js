b73322d440c9d5f29d282d21037a8bca8e94e375
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-06-07 18:21:13 +0200

    Add script for setting up config.json file

---
+++ create-config.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const clientId = process.env.CLIENT_ID;
+const apiKey = process.env.API_KEY;
+
+const data = `{
+  "clientId": ${clientId}",
+  "apiKey": "${apiKey}"
+}
+`;
+
+fs.writeFileSync("src/config.json", data);
